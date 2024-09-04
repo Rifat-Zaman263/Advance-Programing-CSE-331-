5b26bcff0fa98a63b114599780f32ecfd465e1c8
Rifat Zaman <gamerrifat483@gmail.com>
2024-09-04 14:34:39 +0600

    Update task1.java

--- Lab02/task1.java
+++ Lab02/task1.java
@@ -1,1 +1,29 @@
 
+import java.util.Scanner;
+public class task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        boolean isPrime = true;
+        if (number <= 1) {
+            isPrime = false;
+        } else if (number <= 3) {
+            isPrime = true;
+        } else if (number % 2 == 0 || number % 3 == 0) {
+            isPrime = false;
+        } else {
+            for (int i = 5; i * i <= number; i += 6) {
+                if (number % i == 0 || number % (i + 2) == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}
