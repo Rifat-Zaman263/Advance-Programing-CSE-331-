8cebbf81aa6db258a5e1c92dfb75c27a41ebc65e
Rifat Zaman <gamerrifat483@gmail.com>
2024-09-04 15:17:52 +0600

    Create Task5.java

---
+++ Lab02/Task5.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Number of rows: ");
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
